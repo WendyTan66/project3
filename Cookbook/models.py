9ce35a4c92086c1e2f01d42343911b38439b9f0e
WendyTan66 <wendytanvalencia@gmail.com>
2025-02-14 00:30:40 +0000

    Your commit message

--- Cookbook/models.py
+++ Cookbook/models.py
@@ -1,14 +1,20 @@
 from Cookbook import db
 
-class User(db.Model):
+class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    recipes = db.relationship('Recipe', backref='author', lazy=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    recipes = db.relationship('Recipe', backref='category', cascade="all,delete", lazy=True)
+    def __repr__(self):
+        #represent itself in the form of a string
+        return self.category_name
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        #__repr__to represent itself in the form of a string
+        return f"<Recipe {self.title}>"
--- Cookbook/routes.py
+++ Cookbook/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template
 from Cookbook import app,db
-from Cookbook.models import User, Recipe
+from Cookbook.models import Category, Recipe
+
 
 @app.route("/")
 def home():
     return render_template("cookbook.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET","POST"])
+def add_category():
+    return render_template("add_category.html")
