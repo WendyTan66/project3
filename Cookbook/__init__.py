75f47df11534b496bb7bbc64c9d7141fcf5bf8a4
Wendy Tan <wendytanvalencia@gmail.com>
2025-02-24 00:51:06 +0000

    Depolyment preparation

--- Cookbook/__init__.py
+++ Cookbook/__init__.py
@@ -11,6 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 if os.environ.get("DEVELOPMENT") == "True":
     # If in development mode, use the local database URL (DB_URL)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
@@ -21,11 +22,10 @@ else:
         # If the URL uses the old postgres:// format, change it to postgresql://
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
-    
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
 from Cookbook import routes
 from Cookbook import models
-
