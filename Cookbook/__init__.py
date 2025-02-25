bce8ac2f96b9003c307332d2fb9bcac7b8403fe0
Wendy Tan <wendytanvalencia@gmail.com>
2025-02-25 03:11:49 +0000

    table is back

--- Cookbook/__init__.py
+++ Cookbook/__init__.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from pathlib import Path
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
